5fd52ceb38040441db5c904e1ef54e6e122c515f
ruaryb <bogdan.ryazhin@raiffeisen.ru>
2025-11-10 18:17:05 +0300

    init commit

---
+++ src/main/kotlin/ru/raif/dddarchraif/DddArchRaifApplication.kt
@@ -0,0 +1,11 @@
+package ru.raif.dddarchraif
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DddArchRaif1Application
+
+fun main(args: Array<String>) {
+    runApplication<DddArchRaif1Application>(*args)
+}
---
+++ src/test/kotlin/ru/raif/dddarchraif/DddArchRaifApplicationTests.kt
@@ -0,0 +1,13 @@
+package ru.raif.dddarchraif
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DddArchRaifApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
