a91d7b7b3a41e3e841fa665ad6bbab61329c44a0
ruaryb <bogdan.ryazhin@raiffeisen.ru>
2025-11-23 12:56:56 +0300

    module-2 code

--- src/main/kotlin/ru/raif/delivery/DeliveryApplication.kt
+++ src/main/kotlin/ru/raif/delivery/DeliveryApplication.kt
@@ -1,7 +1,10 @@
 package ru.raif.delivery
 
+import arrow.core.Either
+import arrow.core.right
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import ru.raif.delivery.core.domain.model.shared.Location
 
 @SpringBootApplication
 class DeliveryApplication
