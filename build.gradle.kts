f327bc04058d8b36054b01bd790739f168c2cf1e
ruaryb <bogdan.ryazhin@raiffeisen.ru>
2025-11-23 12:56:56 +0300

    module-2 code

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,8 +23,10 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("io.arrow-kt:arrow-core:2.2.0")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+    testImplementation("io.mockk:mockk:1.14.3")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
